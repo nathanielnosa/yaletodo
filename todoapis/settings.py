eac803fea0b7a3db66bd0600985be298470d52e1
Nathaniel Nosa <nathanielnarth@gmail.com>
2025-11-07 14:09:15 +0100

    re deploy

--- todoapis/settings.py
+++ todoapis/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ly2u9w((j&07ab06z5l@$3_g=al)&r-*^-6$cr-+vn*c+baznh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000","*","localhost","http://cocun.com","https://cocun.com"]
+ALLOWED_HOSTS = ["https://yaletodo.onrender.com","yaletodo.onrender.com"]
 
 
 # Application definition
